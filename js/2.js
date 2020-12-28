93c792b4d0aaa825654b0d06c3d265ef4d5ebebf
Emilio Hurtado <emiliohurtado1@gmail.com>
2020-12-28 02:17:33 -0300

    tratando de hacer ejercicio 2

---
+++ js/2.js
@@ -0,0 +1,8 @@
+let ciudades = [];
+let usuario;
+while (usuario == true) {
+    for (let i = 0; i == ciudades.length; i++) {
+        usuario = prompt("Ingrese ciudades");
+        ciudades.push[usuario];
+    }
+}
